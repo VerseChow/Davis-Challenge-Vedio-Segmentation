f074d6b1f2ed41ded4b6f7f9f85a21e9f52d1171
Manikandasriram <srmani@umich.edu>
2017-04-07 10:46:19 -0400

    minor corrections

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main(config):
     x, y = input_pipeline(filename_queue, config.batch_size, WIDTH[config.res], HEIGHT[config.res])
     x = tf.cast(x, tf.float32)
     y = tf.cast(y, tf.float32)
-    if config.training:
+    if config.training == 1:
         logits, loss = build_model(x, y)
 
         learning_rate = tf.placeholder(tf.float32, shape=[], name='lr')
@@ -39,7 +39,7 @@ def main(config):
     result_train = tf.cast(result_train*255, tf.uint8)
     tf.summary.image('result_train_img', x, max_outputs=config.batch_size)
     tf.summary.image('result_train', result_train, max_outputs=config.batch_size)
-    
+
     sum_all = tf.summary.merge_all()
     print('Finished initializing in %.2f seconds.' % (time.time() - t0))
 
@@ -63,17 +63,18 @@ def main(config):
         threads = tf.train.start_queue_runners(coord=coord)
         writer = tf.summary.FileWriter("./logs", sess.graph)
         t0 = time.time()
-        if config.training:
-            total_count = 0
+        total_count = 0
+        if config.training == 1:
+            print("Starting training")
             for epoch in range(config.num_epochs):
                 lr = config.init_learning_rate * config.learning_rate_decay**epoch
 
                 for k in range(DB_LEN[config.set] // config.batch_size):
                     l_train, _, a = sess.run([loss, train_step, y], feed_dict={learning_rate: lr})
-                    
+
                     if total_count % (20) == 0:
                         writer.add_summary(sess.run(sum_all, feed_dict={learning_rate: lr}), total_count)
-                    total_count += 1                
+                    total_count += 1
                     m, s = divmod(time.time() - t0, 60)
                     h, m = divmod(m, 60)
                     print('Epoch: [%4d/%4d], [%4d/%4d], Time: [%02d:%02d:%02d], loss: %.4f'
@@ -83,13 +84,15 @@ def main(config):
                     print('Saving checkpoint ...')
                     saver.save(sess, './checkpoint/Davis.ckpt', global_step=epoch)
         else:
+            print("Starting validation")
             for k in range(2079 // config.batch_size):
-                writer.add_summary(sess.run(sum_all), total_count)
+                summary, l_val = sess.run([sum_all, loss])
+                writer.add_summary(summary, total_count)
                 total_count += 1
                 m, s = divmod(time.time() - t0, 60)
                 h, m = divmod(m, 60)
                 print('Batch: [%4d/%4d], Time: [%02d:%02d:%02d], loss: %.4f'
-                % (k, DB_LEN[config.set] // config.batch_size, h, m, s, l_train))
+                % (k, DB_LEN[config.set] // config.batch_size, h, m, s, l_val))
         coord.request_stop()
         coord.join(threads)
 
@@ -98,7 +101,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='DAVIS Video Object Segmentation')
     parser.add_argument('--batch-size', dest='batch_size', help='number of images in each batch', default=4, type=int)
     parser.add_argument('--num-epochs', dest='num_epochs', help='total number of epochs to run for training', default=100, type=int)
-    parser.add_argument('--training', dest='training', help='if true, train the model; otherwise evaluate the existing model', default=True, type=bool)
+    parser.add_argument('--training', dest='training', help='if true, train the model; otherwise evaluate the existing model', default=1, type=int)
     parser.add_argument('--init-lr', dest='init_learning_rate', help='initial learning rate', default=1e-4, type=float)
     parser.add_argument('--lr-decay', dest='learning_rate_decay', help='ratio of decaying the learning rate after each epoch', default=0.95, type=float)
     parser.add_argument('--gpu', dest='gpu', help='GPU device id to be used', default='0', type=str)
